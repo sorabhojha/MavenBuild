d803e0597f556e958315fee847e74906bafd4a71
sorabhojha <sorabhojha@gmail.com>
2018-05-17 08:43:38 +0530

    GitMAven First Commit

---
+++ src/main/java/com/nokia/GitMaven/App.java
@@ -0,0 +1,16 @@
+package com.nokia.GitMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        for(int i=1; i<=10; i++){
+			System.out.println("This is a java program "+i);
+        }
+    }
+}
